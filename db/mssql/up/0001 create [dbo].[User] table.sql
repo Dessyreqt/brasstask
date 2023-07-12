f45c32f0e547c4dfcb593ac823c3f6b72eb2eebd
David Carroll <carroll.david.s@gmail.com>
2023-07-12 13:43:39 -0500

    Set unique constraints on username and email

--- db/mssql/up/0001 create [dbo].[User] table.sql
+++ db/mssql/up/0001 create [dbo].[User] table.sql
@@ -1,9 +1,11 @@
 CREATE TABLE [dbo].[User] (
     [UserId] UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
     [Username] VARCHAR(50) NOT NULL,
-    [Email] VARCHAR(MAX) NOT NULL,
+    [Email] VARCHAR(254) NOT NULL,
     [PasswordSalt] VARCHAR(100) NOT NULL,
     [PasswordHash] VARCHAR(100) NOT NULL,
     [CreatedAt] DATETIME DEFAULT GETDATE(),
-    [UpdatedAt] DATETIME DEFAULT GETDATE()
+    [UpdatedAt] DATETIME DEFAULT GETDATE(),
+    CONSTRAINT [UQ_Users_Username] UNIQUE ([Username]),
+    CONSTRAINT [UQ_Users_Email] UNIQUE ([Email])
 );
