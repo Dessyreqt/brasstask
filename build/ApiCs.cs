7463120c5c90394a39245c7f8c6c895514e1c665
David Carroll <carroll.david.s@gmail.com>
2023-07-17 22:49:30 -0500

    Add target to setup user secrets

---
+++ build/ApiCs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+
+partial class Build
+{
+    Target SetupUserSecrets =>
+        _ => _
+            .Executes(() =>
+            {
+                var secrets = DotNetTasks.DotNet($"user-secrets list", Paths.ApiCsProject);
+
+                SetUserSecretIfMissing(secrets, "Serilog:WriteTo:2:Args:apiKey", "<replace with Serilog API key>");
+                SetUserSecretIfMissing(secrets, "Jwt:Key", Guid.NewGuid().ToString("N"));
+            });
+
+    void SetUserSecretIfMissing(IReadOnlyCollection<Output> secrets, string secretName, string secretValue)
+    {
+        if (!secrets.Any(x => x.Text.StartsWith(secretName)))
+        {
+            DotNetTasks.DotNet($"user-secrets set \"{secretName}\" \"{secretValue}\"", Paths.ApiCsProject);
+        }
+    }
+}
--- build/Paths.cs
+++ build/Paths.cs
@@ -7,6 +7,9 @@ public class Paths
     public static readonly AbsolutePath Tools = NukeBuild.RootDirectory / "bin";
     public static readonly AbsolutePath RoundhouseExe = Tools / "rh.exe";
 
+    // C# API
+    public static readonly AbsolutePath ApiCsProject = NukeBuild.RootDirectory / "api" / "cs" / "BrassTask.Api";
+
     // T-SQL database files
     public static readonly AbsolutePath Mssql = NukeBuild.RootDirectory / "db" / "mssql";
     public static readonly AbsolutePath CreateDbScriptMssql = Mssql / "create.sql";
